bfab03576256bec28044cdc28643db498d6982f3
Manuel Alejandro <mplaza005@gmail.com>
2026-06-30 11:03:36 -0500

    product

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+    public function create(){
+
+    return view('category.create');
+
+    }
+
+
+    public function store(Request $request){
+
+        $category=new Category();
+
+        $category->name=$request->name;
+
+        $category->save();
+
+        return $category;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SaludosController;
 use App\Http\Controllers\OperacionesController;
-
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,19 +16,17 @@ use App\Http\Controllers\OperacionesController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/saludo', function () {
-    return view('saludoht');
-});
+Route::get('/product/create',[ProductController::class,'create']);
+Route::post('/product/store',[ProductController::class,'store'])->name('product.store');
 
 
 
+
+Route::get('/category/create',[CategoryController::class,'create']);
+Route::post('/category/store',[CategoryController::class,'store'])->name('category.store');
 
 
 Route::get('/frm_sumar',[OperacionesController::class,'frm_sumar']);
 Route::post('/sumar',[OperacionesController::class,'sumar'])->name('sumar.store');
 
-Route::get('/restar',[OperacionesController::class,'restar']);
